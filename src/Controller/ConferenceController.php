12d92db47a5b8e180867d03e448ddf5715f45203
Clement-Rey07 <clement.rey2@etu.univ-st-etienne.fr>
2026-01-04 11:08:58 +0100

    Initialisation du Portfolio Symfony

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,30 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ConferenceController extends AbstractController
 {
-    #[Route('/home', name: 'homepage')]
-    public function index(string $name = ''): Response
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
     {
-        $greet = '';
-        if ($name) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-        return new Response(<<<EOF
-            <html>
-                <body>
-                    $greet
-                    <img src="images/under-construction2.avif" />
-                </body>
-            </html>
-            EOF
-        );
+        return $this->render('home/index.html.twig');
     }
-HEAD
 }
-}
-c5263b4 (Your commit message)
