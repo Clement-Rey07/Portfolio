78f6ac6c666df6e9d2c46ab32ecf93e37acd4742
Clement-Rey07 <clement.rey2@etu.univ-st-etienne.fr>
2026-01-06 17:22:12 +0100

    Correction fautes 2.0

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -28,20 +28,18 @@ final class ConferenceController extends AbstractController
         return $this->render('contact/index.html.twig');
     }
 
-    // FORMULAIRE AVANT TÉLÉCHARGEMENT (Sans Mailer, juste redirection)
+
     #[Route('/download-request', name: 'app_download_request')]
     public function downloadRequest(Request $request): Response
     {
         if ($request->isMethod('POST')) {
-            // Ici, on pourrait traiter les données, mais on respecte le "Sans BDD"
-            // On déclenche directement le téléchargement du fichier réel
             return $this->file($this->getParameter('kernel.project_dir') . '/public/cv/mon_cv.pdf');
         }
 
         return $this->render('download/index.html.twig');
     }
 
-    // GÉNÉRATEUR DE CV (L'autre demande de ton cahier des charges)
+
     #[Route('/registration', name: 'registration')]
     public function registration(Request $request): Response
     {
@@ -50,7 +48,7 @@ final class ConferenceController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
-            // On affiche une page qui ressemble à un CV avec les données saisies
+
             return $this->render('registration/cv.html.twig', ['data' => $data]);
         }
 
