19c4c46c1eed2e6403cd32d940e944a966deae4a
Clement-Rey07 <clement.rey2@etu.univ-st-etienne.fr>
2026-01-06 10:31:23 +0100

    Ajout du bouton de téléchargement et du fichier PDF

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,30 +2,31 @@
 
 namespace App\Controller;
 
-use App\Form\CvFormType;
+use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class RegistrationController extends AbstractController
+final class RegistrationController extends AbstractController
 {
-    #[Route('/inscription', name: 'inscription')]
+    #[Route('/registration', name: 'registration')]
     public function index(Request $request): Response
     {
-        $form = $this->createForm(CvFormType::class);
+        $form = $this->createForm(RegistrationType::class);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
 
-            return $this->render('cv/preview.html.twig', [
-                'data' => $data,
+            return $this->render('registration/cv.html.twig', [
+                'data' => $data
             ]);
         }
 
         return $this->render('registration/index.html.twig', [
-            'form' => $form->createView(),
+            'form' => $form->createView()
         ]);
     }
 }
